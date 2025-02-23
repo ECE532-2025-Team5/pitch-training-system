8618f3b9616abce2474ef0266b3b2437ba84df1f
Angus Wu <wuangusjh@gmail.com>
2025-02-23 12:41:25 -0500

    removed unnecessary comments from keyboard.v

--- keyboard/keyboard.v
+++ keyboard/keyboard.v
@@ -1,27 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: Digilent Inc.
-// Engineer: Thomas Kappenman
-// 
-// Create Date: 03/03/2015 09:06:31 PM
-// Design Name: 
-// Module Name: top
-// Project Name: Nexys4DDR Keyboard Demo
-// Target Devices: Nexys4DDR
-// Tool Versions: 
-// Description: This project takes keyboard input from the PS2 port,
-//  and outputs the keyboard scan code to the 7 segment display on the board.
-//  The scan code is shifted left 2 characters each time a new code is
-//  read.
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module keyboard_ps2(
     input clk,
